f74db7fa1a42b1de9ddbb7922ac8e0fad82d92f3
MarcoAlvarez704 <poet14m@gmail.com>
2025-07-07 20:44:29 -0600

    Update Distancia Manhattan.py

--- Distancia Manhattan.py
+++ Distancia Manhattan.py
@@ -1,3 +1,5 @@
+#Distancia Manhattan
+#Fórmula: d = |x2 - x1| + |y2 - y1|
 print("Programa para calcular la distancia Manhattan entre p1(x1, y1) y p2(x2, y2).")
 
 # Se le solicita al usuario ingresar los datos de las coordenadas
@@ -11,9 +13,8 @@ x2 = float(input("x2: "))
 y2 = float(input("y2: "))
 p2 = (x2, y2)
 
-# Se calcula la distancia Manhattan con la siguiente fórmula: d = |x2 - x1| + |y2 - y1|
+# Se calcula la distancia Manhattan con la formula
 distancia_manhattan = abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
-# Se agrega abs para el valor absoluto
 
 # Se utiliza f-strings para un formato correcto en el resultado
 print(f"\nLa distancia Manhattan entre {p1} y {p2} es: {distancia_manhattan:.2f}")
